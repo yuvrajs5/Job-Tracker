0bd07f33cd3fd1a04fd1a48f08bf0a6ce050864d
yuvrajs5 <154221677+yuvrajs5@users.noreply.github.com>
2025-07-30 16:41:01 +0530

    cors update

--- api/server.js
+++ api/server.js
@@ -16,31 +16,33 @@ import connectDB from './db/connect.js';
 import cors from 'cors';
 
 const allowedOrigins = [
-  'http://localhost:5173', // for local dev
-  'https://job-tracker-7s7w40psm-yuvraj-singhs-projects-07fb547b.vercel.app', // your Vercel URL
+  'http://localhost:5173', // for dev
+  'https://job-tracker-7s7w40psm-yuvraj-singhs-projects-07fb547b.vercel.app', // vercel frontend
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.options('*', cors());
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
-  res.setHeader('Access-Control-Expose-Headers', 'auth'); // Expose the 'auth' header to the client
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
+//   res.setHeader('Access-Control-Expose-Headers', 'auth'); // Expose the 'auth' header to the client
+//   next();
+// });
 
 
 app.use(express.urlencoded({ extended: true }));
